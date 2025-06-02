b28b13b4999c8a2cd46cf3ca7e4ef857777eec59
Mirsibkhan <agaevsubxan@gmail.com>
2025-06-02 13:17:38 +0300

    Bag fixed

--- interfaces_adapters/ports_impl/tr_image_text_extractor.py
+++ interfaces_adapters/ports_impl/tr_image_text_extractor.py
@@ -11,11 +11,11 @@ class TrImageTextExtractor(ImageTextExtractor):
     def __init__(self):
         self.executor = ThreadPoolExecutor(max_workers=4)
 
-        self.h_processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-handwritten').to("cuda")
-        self.h_model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-handwritten').to("cuda")
+        self.h_processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-handwritten')
+        self.h_model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-handwritten')
 
-        self.p_processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-printed').to("cuda")
-        self.p_model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-printed').to("cuda")
+        self.p_processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-printed')
+        self.p_model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-printed')
 
     async def image_to_text_handwritten(
             self,
@@ -43,7 +43,9 @@ class TrImageTextExtractor(ImageTextExtractor):
         pixel_values = self.h_processor(images=image, return_tensors="pt").pixel_values
 
         generated_ids = self.h_model.generate(pixel_values)
-        generated_text = self.h_processor.batch_deocde(generated_ids, skip_special_tokens=False)[0]
+        generated_text = self.h_processor.batch_decode(generated_ids, skip_special_tokens=False)[0]
+
+        print(generated_text)
 
         return generated_text
 
@@ -51,6 +53,8 @@ class TrImageTextExtractor(ImageTextExtractor):
         pixel_values = self.p_processor(images=image, return_tensors="pt").pixel_values
 
         generated_ids = self.p_model.generate(pixel_values)
-        generated_text = self.p_processor.batch_deocde(generated_ids, skip_special_tokens=False)[0]
+        generated_text = self.p_processor.batch_decode(generated_ids, skip_special_tokens=False)[0]
+
+        print(generated_text)
 
         return generated_text
